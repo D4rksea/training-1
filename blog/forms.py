42108f9a603ede4de79e7238485846af7969b601
D4rksea <65219077+D4rksea@users.noreply.github.com>
2020-05-14 10:38:55 +0200

    aggiunti i commenti

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,13 @@
 from django import forms
-from .models import BlogPostModel
-
+from .models import BlogPostModel, BlogCommentModel
 
 class BlogPostModelForm(forms.ModelForm):
-
     class Meta:
         model = BlogPostModel
         #fields = "__all__"
-        fields = ["titolo","contenuto", 'bozza']
+        fields = ["titolo","contenuto", "bozza"]
+
+class BlogCommentModelForm(forms.ModelForm):
+    class Meta:
+        model = BlogCommentModel
+        fields = ['contenuto']
